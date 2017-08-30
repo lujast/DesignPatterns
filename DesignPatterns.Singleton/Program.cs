03fa8cfe565093b3a331b13eda37299a6c75ff85
franz maurer <adnil997@wp.pl>
2017-08-30 13:19:21 +0200

    singleton

---
+++ DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = Singleton.GetInstance();
+
+
+            Console.WriteLine(s1.GetNumber());
+            Console.WriteLine(s1.GetDateTime());
+
+            System.Threading.Thread.Sleep(2000);
+            var s2 = Singleton.GetInstance();
+
+            Console.WriteLine(s2.GetNumber());
+            Console.WriteLine(s2.GetDateTime());
+
+
+
+        }
+    }
+}
---
+++ DesignPatterns.Singleton/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        private int number;
+        private DateTime datetime;
+        private static Singleton instance = new Singleton();
+
+        private Singleton()
+        {
+            this.number = new Random().Next(0,100);
+            this.datetime = DateTime.Now;
+        }
+
+        public static Singleton GetInstance()
+        {
+            return instance;
+        }
+
+        public int GetNumber()
+        {
+            return this.number;
+        }
+
+        public DateTime GetDateTime()
+        {
+            return this.datetime;
+        }
+
+    }
+}
